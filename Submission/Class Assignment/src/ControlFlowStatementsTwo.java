74bf4be6578fc6edd3d26c8e3b86d1a8638ca93e
FionaKanyi <93208460+FionaKanyi@users.noreply.github.com>
2022-04-29 15:01:17 +0100

    Delete Submission directory

--- Submission/Class Assignment/src/ControlFlowStatementsTwo.java
+++
@@ -1,6 +0,0 @@
-public class ControlFlowStatementsTwo {
-    public static void main(String[] args) {
-        ControlFlowStatementsOne controlFlowStatementsOne = new ControlFlowStatementsOne();
-        controlFlowStatementsOne.showControlFlowStatements();
-    }
-}
